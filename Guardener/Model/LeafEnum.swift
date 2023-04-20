ab5167424a42f20a894ede7cd8d93015bc820618
Omkar Nikhal <69253915+gitpushOmnik@users.noreply.github.com>
2023-04-19 17:55:00 -0700

    Add Leaf Enum
    Leaf Disease Representation using Swift Enum

---
+++ Guardener/Model/LeafEnum.swift
@@ -0,0 +1,30 @@
+//
+//  LeafEnum.swift
+//  Guardener
+//
+//  Created by Omkar Nikhal.
+//
+
+import Foundation
+
+enum LeafDisease {
+    
+    case appleScab
+    case appleBlackRot
+    case appleRust
+    case healthy
+    case powderyMildew
+    case bacterialLeafSpot
+    case commonRust
+    case leafBlight
+    case grapeBlackRot
+    case earlyBlight
+    case lateBlight
+    case leafScorch
+    case leafMold
+    case targetSpot
+    case mosaicVirus
+    case leafCurl
+    case spiderMites
+    case orangeHaunglongbing
+}
